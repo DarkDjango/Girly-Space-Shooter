739896558f80db21f4639b7af9a19350943a888d
DarkDjango <dark.django@hotmail.com>
2018-05-08 01:16:26 -0300

    1 - Versão Inicial
    Primeira versão rudimentar do jogo. Possui:
    - Personagem jogável, capaz de se movimentar, disparar, e morrer
    - Inimigos capazes de se movimentar e morrer
    - Animação no jogador e nos inimigos

---
+++ Assets/scripts/DeathAnScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathAnScript : MonoBehaviour {
+	public Transform boomPrefab;
+	public float Timer;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void OnDestroy()
+	{
+		var boomTransform = Instantiate(boomPrefab) as Transform;
+		BoomScript boom = boomTransform.gameObject.GetComponent<BoomScript>();
+		// Assign position
+		boomTransform.position = transform.position;
+		if (boom != null)
+		{
+			boom.timer = Timer;
+		}
+	}
+}
---
+++ Assets/scripts/PlayerScript.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour {
+
+
+
+	/// 1 - The speed of the ship
+	public Vector2 speed = new Vector2(25, 25);
+
+	// 2 - Store the movement and the component
+	private Vector2 movement;
+	private Rigidbody2D rigidbodyComponent;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// 3 - Retrieve axis information
+		float inputX = Input.GetAxis("Horizontal");
+		float inputY = Input.GetAxis("Vertical");
+
+		// 4 - Movement per direction
+		movement = new Vector2(
+			speed.x * inputX,
+			speed.y * inputY);
+		// 5 - Shooting
+		bool shoot = Input.GetButton("Fire1");
+		shoot |= Input.GetButton("Fire2");
+
+		if (shoot)
+		{
+			WeaponScript weapon = GetComponent<WeaponScript>();
+			if (weapon != null)
+			{
+				// false because the player is not an enemy
+				weapon.Attack(false);
+			}
+		}
+	}
+
+	void FixedUpdate()
+	{
+		// 6 - Get the component and store the reference
+		if (rigidbodyComponent == null) rigidbodyComponent = GetComponent<Rigidbody2D>();
+
+		// 7 - Move the game object
+		rigidbodyComponent.velocity = movement;
+	}
+
+	void OnCollisionEnter2D(Collision2D collision)
+	{
+		bool damagePlayer = false;
+
+		// Collision with enemy
+		EnemyScript enemy = collision.gameObject.GetComponent<EnemyScript>();
+		if (enemy != null)
+		{
+			// Kill the enemy
+	//		HealthScript enemyHealth = enemy.GetComponent<HealthScript>();
+	//		if (enemyHealth != null) enemyHealth.Damage(enemyHealth.hp);
+
+			damagePlayer = true;
+		}
+
+		// Damage the player
+		if (damagePlayer)
+		{
+			HealthScript playerHealth = this.GetComponent<HealthScript>();
+			if (playerHealth != null) playerHealth.Damage(1);
+		}
+	}
+}
